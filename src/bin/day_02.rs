716eb8eac0c8a6af942da56784365eabae0ce768
tkr3 <git@tkr3.de>
2025-12-02 21:14:03 +0100

    day_02 part 1

---
+++ src/bin/day_02.rs
@@ -0,0 +1,101 @@
+pub fn part1(input: &str) -> u64 {
+    input
+        .trim()
+        .split(',')
+        .filter_map(|x| {
+            let (a, b) = x.split_once('-').unwrap();
+
+            if a.len() % 2 == 1 && b.len() == a.len() {
+                return None;
+            }
+            Some((a, b))
+        })
+        .flat_map(|(a, b)| {
+            let start: u64 = if a.len() % 2 == 0 {
+                let (a1, a2) = a.split_at(a.len() / 2);
+                let a1 = a1.parse().unwrap();
+                let a2 = a2.parse::<u64>().unwrap();
+                if a2 > a1 {
+                    a1 + 1
+                } else {
+                    a1
+                }
+            } else {
+                10u64.pow(a.len() as u32 / 2)
+            };
+
+            let end: u64 = if b.len() % 2 == 0 {
+                let (b1, b2) = b.split_at(b.len() / 2);
+                let b1 = b1.parse().unwrap();
+                let b2 = b2.parse::<u64>().unwrap();
+                if b2 >= b1 {
+                    b1
+                } else {
+                    b1 - 1
+                }
+            } else {
+                10u64.pow(b.len() as u32 / 2) - 1
+            };
+
+            start..=end
+        })
+        .map(|x| x * 10u64.pow(x.ilog10() + 1) + x)
+        .sum()
+}
+
+pub fn part2(input: &str) -> i32 {
+    // if a.len() % 2 == 0 {
+    //     if a.len() == b.len() {
+    //         let start: u64 = a[..a.len() / 2].parse().unwrap();
+    //         let end: u64 = b[..b.len() / 2].parse().unwrap();
+    //
+    //         end - start
+    //     } else {
+    //         let start: u64 = a[..a.len() / 2].parse().unwrap();
+    //         let end = 10u64.pow(b.len() as u32 / 2 + 1);
+    //
+    //         end - start
+    //     }
+    // } else if b.len() % 2 == 0 {
+    //     let start = 10u64.pow(b.len() as u32 / 2 - 1);
+    //     let end: u64 = b[..b.len() / 2].parse().unwrap();
+    //
+    //     end - start
+    // } else {
+    //     0
+    // }
+    0
+}
+
+const INPUT: &str = include_str!("../../inputs/day_02.txt");
+
+fn main() {
+    let p1 = part1(INPUT);
+    println!("Part 1: {p1}");
+
+    let p2 = part2(INPUT);
+    println!("Part 2: {p2}");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_INPUT_1: &str = "\
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,\
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,\
+824824821-824824827,2121212118-2121212124\
+";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part1(EXAMPLE_INPUT_1), 1_1227775554);
+    }
+
+    const EXAMPLE_INPUT_2: &str = EXAMPLE_INPUT_1;
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part2(EXAMPLE_INPUT_2), 6);
+    }
+}
