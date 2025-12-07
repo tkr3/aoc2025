085c40f761d446ad856d0d00f8a7ef545b2a3742
tkr3 <git@tkr3.de>
2025-12-07 14:58:32 +0100

    day_06 part 2

--- src/bin/day_06.rs
+++ src/bin/day_06.rs
@@ -35,8 +35,48 @@ pub fn part1(input: &str) -> u64 {
     unreachable!("no operators line");
 }
 
-pub fn part2(input: &str) -> u32 {
-    0
+pub fn part2(input: &str) -> u64 {
+    let lines: Vec<&str> = input.lines().collect();
+    let operator_line = lines.last().unwrap();
+    let operators: Vec<(usize, char)> = operator_line
+        .char_indices()
+        .filter(|x| !x.1.is_whitespace())
+        .collect();
+
+    let mut numbers: Vec<[String; 5]> = vec![Default::default(); operators.len()];
+
+    for line in lines[..lines.len() - 1].iter() {
+        let mut column_index = 0;
+        let mut columns = operators.iter().map(|x| x.0);
+        let mut current_column = columns.next().unwrap();
+        let mut next_column = columns.next().unwrap();
+        for (index, char) in line.char_indices() {
+            if index >= next_column {
+                current_column = next_column;
+                next_column = columns.next().unwrap_or(usize::MAX);
+                column_index += 1;
+            }
+            if char.is_whitespace() {
+                continue;
+            }
+            numbers[column_index][index - current_column].push(char);
+        }
+    }
+
+    numbers
+        .into_iter()
+        .map(|x| {
+            x.iter()
+                .filter_map(|y| y.parse::<u64>().ok())
+                .collect::<Vec<_>>()
+        })
+        .zip(operators.into_iter().map(|(_, operator)| match operator {
+            '*' => std::ops::Mul::mul,
+            '+' => std::ops::Add::add,
+            _ => unreachable!(),
+        }))
+        .map(|(numbers, operator)| numbers.into_iter().reduce(operator).unwrap())
+        .sum()
 }
 
 const INPUT: &str = include_str!("../../inputs/day_06.txt");
@@ -69,6 +109,6 @@ mod test {
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part2(EXAMPLE_INPUT_2), 1);
+        assert_eq!(part2(EXAMPLE_INPUT_2), 3263827);
     }
 }
